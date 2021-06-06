930bd8a9ced28f0e2aae9688491de0fe7bb120ae
shoNagai <strawberry4062@gmail.com>
2021-06-06 20:09:00 +0900

    refactor: 💡 use import cloud/firestore

--- src/firestoreDataLoader.ts
+++ src/firestoreDataLoader.ts
@@ -1,6 +1,6 @@
+import { CollectionReference, FieldPath, Query } from "@google-cloud/firestore";
 import { DataSource } from "apollo-datasource";
 import DataLoader, { Options } from "dataloader";
-import { firestore } from "firebase-admin";
 import { getDocData } from "./utils/getDocData";
 
 /**
@@ -16,7 +16,7 @@ export class FirestoreDataLoader<TDocument = any> extends DataSource {
    * @param options DataLoader.Options. maxBatchSize is set to 10 due to firestore limitation.
    */
   constructor(
-    baseQuery: firestore.CollectionReference | firestore.Query,
+    baseQuery: CollectionReference | Query,
     options?:
       | Options<
           string,
@@ -38,12 +38,12 @@ export class FirestoreDataLoader<TDocument = any> extends DataSource {
 
   /**
    * load datas by keys
-   * @param {firestore.CollectionReference | firestore.Query} baseQuery CollectionReference or Query
+   * @param {CollectionReference | Query} baseQuery CollectionReference or Query
    * @param {string[]} keys
    * @returns documents
    */
   async loadDataByKeys(
-    baseQuery: firestore.CollectionReference | firestore.Query,
+    baseQuery: CollectionReference | Query,
     keys: readonly string[]
   ): Promise<
     (
@@ -53,8 +53,7 @@ export class FirestoreDataLoader<TDocument = any> extends DataSource {
       | null
     )[]
   > {
-    console.log("Call loadDataByIds", keys);
-    const snapshot = await baseQuery.where(firestore.FieldPath.documentId(), `in`, keys).get();
+    const snapshot = await baseQuery.where(FieldPath.documentId(), `in`, keys).get();
     if (snapshot.empty) return [];
     return keys.map((key) => {
       const doc = snapshot.docs.find((doc) => doc.id === key);
